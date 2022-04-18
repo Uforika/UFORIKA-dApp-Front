ff26326bc38b770835389442b3cf2c6defa957b2
Viktor <viktor.pulyak@pixelplex.io>
2022-04-18 16:43:06 +0300

    [UF-74] Add client sdk build

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { ToastContainer } from 'react-toastify';
-import { SWRConfig } from 'swr';
 import ReactModal from 'react-modal';
 import { useSocket } from '@hooks/socket.hooks';
 import '@assets/scss/global.scss';
@@ -10,6 +9,10 @@ import { AppProps } from 'next/app';
 import { ModalProvider } from '@contexts/modals.context';
 import { AuthProvider } from '@contexts/auth.context';
 import { WalletProvider } from '@contexts/wallet.context';
+import { SDKConfig } from '@782-uforika/client-sdk';
+import { TSDKConfig } from '@782-uforika/client-sdk/core/ConfigContext';
+import { CONFIG } from '@constants/config.constants';
+import { SDK_PATH_TO_JWT, SDK_REFRESH_TOKEN_REQUEST, SDK_SIGN_IN_PATHS } from '@constants/sdk.constants';
 
 ReactModal.setAppElement('#__next');
 
@@ -20,9 +23,19 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   }
 
   return (
-    <SWRConfig
+    <SDKConfig
       value={{
+        baseApiUrl: CONFIG.API_URL,
+        refreshToken: {
+          signInPaths: SDK_SIGN_IN_PATHS,
+          pathToJWT: SDK_PATH_TO_JWT,
+          refreshTokenRequest: SDK_REFRESH_TOKEN_REQUEST,
+        },
+      } as TSDKConfig}
+      swrConfig={{
+        revalidateIfStale: false,
         revalidateOnFocus: false,
+        shouldRetryOnError: false,
       }}
     >
       <WalletProvider>
@@ -43,7 +56,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
           </ModalProvider>
         </AuthProvider>
       </WalletProvider>
-    </SWRConfig>
+    </SDKConfig>
   );
 };
 
