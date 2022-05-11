fa71bba81a14f6da5f127421221763de2f69096f
Malkovich <m.malkovich@pixelplex.io>
2022-05-11 20:03:05 +0300

    [UF-77] Add Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,23 +1,17 @@
-import React, { memo, ReactNode } from 'react';
+import React, { memo } from 'react';
 import cn from 'classnames';
 import { Loader, Button as UIButton, StrictButtonProps } from 'semantic-ui-react';
 import styles from './styles.module.scss';
 
 type PickedTypes = 'primary' | 'secondary' | 'children' | 'onClick'| 'loading' | 'className' | 'disabled' | 'size'
 
-type Props = Pick<StrictButtonProps, PickedTypes> & {
-  iconLeft?: ReactNode,
-  iconRight?: ReactNode,
-}
+type Props = Pick<StrictButtonProps, PickedTypes>
 
 const Button: React.FC<Props> = ({
   className,
-  disabled,
   children,
-  iconLeft,
-  iconRight,
-  size = 'large',
   loading,
+  size = 'large',
   ...props
 }) => (
   <div className={styles.buttonWrapper}>
@@ -26,21 +20,13 @@ const Button: React.FC<Props> = ({
         styles.button,
         className,
       )}
-      disabled={disabled || loading}
       size={size}
       {...props}
     >
-      {iconLeft && <div className={styles.iconLeft}>{iconLeft}</div>}
-      {loading && <div className={styles.iconLeft}><Loader className={styles.loader} active /></div>}
+      {loading && <Loader className={styles.loader} active />}
       {children}
-      {iconRight && <div className={styles.iconRight}>{iconRight}</div>}
     </UIButton>
   </div>
 );
-
-Button.defaultProps = {
-  iconLeft: undefined,
-  iconRight: undefined,
-};
 
 export default memo(Button);
