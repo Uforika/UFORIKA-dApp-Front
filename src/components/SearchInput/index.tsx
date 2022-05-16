2d07e827a62dc655eff35b7eac1c94872740aeb4
Maksim Malkovich <m.malkovich@pixelplex.io>
2022-05-16 14:05:42 +0000

    [UF-82] Add Search Input

--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -1,24 +1,21 @@
 import React, {
   memo, FC, useCallback,
 } from 'react';
-import { InputOnChangeData } from 'semantic-ui-react';
-import Input from '@components/Input';
+import {
+  Search as UISearch, SearchProps, StrictSearchProps,
+} from 'semantic-ui-react';
 import { ReactComponent as SearchIcon } from '@assets/images/icons/search.svg';
 import { ReactComponent as CancelIcon } from '@assets/images/icons/cancel.svg';
 import styles from './styles.module.scss';
 
-type Props = {
-  name: string,
-  value: string,
-  onChange: (value: string) => void,
-}
-const SearchInput: FC<Props> = ({
-  onChange,
-  value,
-  ...props
-}) => {
+type OmitTypes = 'onSearchChange'| 'input' | 'icon' | 'open' | 'category' | 'size'
 
-  const handleChange = useCallback((_, data: InputOnChangeData) => {
+type Props = Omit<StrictSearchProps, OmitTypes> & {
+  onChange: (value: string | undefined) => void
+}
+
+const SearchInput: FC<Props> = ({ onChange, ...props }) => {
+  const handleChange = useCallback((_, data: SearchProps) => {
     onChange(data.value);
   }, [onChange]);
 
@@ -28,15 +25,16 @@ const SearchInput: FC<Props> = ({
 
   return (
     <div className={styles.searchContainer}>
-      <SearchIcon className={styles.searchIcon} />
-      <Input className={styles.inputWrapper} onChange={handleChange} value={value} placeholder="Search" {...props} />
-      {value && (
-        <button onClick={handleClearInput} className={styles.cancelButton}>
-          <CancelIcon />
-        </button>
-      )}
+      <UISearch
+        icon={<SearchIcon />}
+        onSearchChange={handleChange}
+        open={false}
+        {...props}
+      />
+      <button onClick={handleClearInput} className={styles.cancelButton}>
+        <CancelIcon />
+      </button>
     </div>
   );
 };
-
 export default memo(SearchInput);
