fda1d0722398d551e7f09db140bc07b6bc47b80c
Maksim Malkovich <m.malkovich@pixelplex.io>
2022-05-26 11:38:41 +0000

    [UF-159] Add title and favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import '@assets/scss/global.scss';
 import 'react-toastify/dist/ReactToastify.css';
 import 'semantic-ui-css/semantic.min.css';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import { ModalProvider } from '@contexts/modals.context';
 import { AuthProvider } from '@contexts/auth.context';
 import { WalletProvider } from '@contexts/wallet.context';
@@ -21,42 +22,55 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   }
 
   return (
-    <SDKConfig
-      value={{
-        baseApiUrl: CONFIG.API_URL,
-        refreshToken: {
-          signInPaths: SDK_SIGN_IN_PATHS,
-          pathToJWT: SDK_PATH_TO_JWT,
-          refreshTokenRequest: SDK_REFRESH_TOKEN_REQUEST,
-        },
-      } as TSDKConfig}
-      swrConfig={{
-        revalidateIfStale: false,
-        revalidateOnFocus: false,
-        shouldRetryOnError: false,
-      }}
-    >
-      <WalletProvider>
-        <AuthProvider>
-          <RateProvider>
-            <ModalProvider>
-              <Component {...pageProps} />
-              <ToastContainer
-                position="top-right"
-                autoClose={3000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-              />
-            </ModalProvider>
-          </RateProvider>
-        </AuthProvider>
-      </WalletProvider>
-    </SDKConfig>
+    <>
+      <Head>
+        <title>Uforika Metaverse</title>
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicons/favicon-16x16.png" />
+        <link rel="manifest" href="/images/favicons/site.webmanifest" />
+        <link rel="mask-icon" href="/images/favicons/safari-pinned-tab.svg" color="#000000" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#000000" />
+      </Head>
+      <SDKConfig
+        value={{
+          baseApiUrl: CONFIG.API_URL,
+          refreshToken: {
+            signInPaths: SDK_SIGN_IN_PATHS,
+            pathToJWT: SDK_PATH_TO_JWT,
+            refreshTokenRequest: SDK_REFRESH_TOKEN_REQUEST,
+          },
+        } as TSDKConfig}
+        swrConfig={{
+          revalidateIfStale: false,
+          revalidateOnFocus: false,
+          shouldRetryOnError: false,
+        }}
+      >
+        <WalletProvider>
+          <AuthProvider>
+            <RateProvider>
+              <ModalProvider>
+                <Component {...pageProps} />
+                <ToastContainer
+                  position="top-right"
+                  autoClose={3000}
+                  hideProgressBar={false}
+                  newestOnTop={false}
+                  closeOnClick
+                  rtl={false}
+                  pauseOnFocusLoss
+                  draggable
+                  pauseOnHover
+                />
+              </ModalProvider>
+            </RateProvider>
+          </AuthProvider>
+        </WalletProvider>
+      </SDKConfig>
+    </>
   );
 };
 
