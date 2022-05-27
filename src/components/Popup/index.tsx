abe63e7d69845e541a042772fa889abbc56f3680
Malkovich <m.malkovich@pixelplex.io>
2022-05-27 14:10:17 +0300

    [UF-170] Disable metamask if don't have extension

--- src/components/Popup/index.tsx
+++ src/components/Popup/index.tsx
@@ -1,11 +1,12 @@
 import React, { FC, memo } from 'react';
 import { Popup as UIPopup, StrictPopupProps } from 'semantic-ui-react';
+import cn from 'classnames';
 import styles from './styles.module.scss';
 
 type Props = Omit<StrictPopupProps, 'inverted' | 'basic'>;
 
-const Popup: FC<Props> = ({ ...props }) => (
-  <UIPopup className={styles.wrap} {...props} />
+const Popup: FC<Props> = ({ className, ...props }) => (
+  <UIPopup className={cn(styles.wrap, className)} {...props} />
 );
 
 export default memo(Popup);
